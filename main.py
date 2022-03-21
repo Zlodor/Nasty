a0ae2a57cf79025bc8af329cb4ae6cff20d733b3
Zlodor <vlad1198@yandex.ru>
2022-03-21 22:18:51 +0400

    secend

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ with open('01_gauss8_0grad.csv', 'r', newline='', encoding='utf-8') as csvfile:
     rows_counter = len(data[0])
     print(f"Строк прочитано - {rows_counter}")
 
-# for m in range(1, int(rows_counter/2)):
-
-
+for m in range(1, int(rows_counter/2) + 1):
+    p = int(rows_counter / m)
+    matrix = []  # Будущая таблица с m столбцов
+    start = 0  # Начало среза
+    for i in range(p):
+        chunk = data[0][start:(start + m)]
+        start += m
+        matrix.append(chunk)
+    # print(f"{m}) p:{p}/{len(matrix)}")
